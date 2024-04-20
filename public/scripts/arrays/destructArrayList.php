8844c764d1997f0126a5bf6e0640d98609354cf8
Bakoulis George <g.bakoulis@outlook.com>
2024-04-20 12:12:58 +0300

    added a list() example

---
+++ public/scripts/arrays/destructArrayList.php
@@ -0,0 +1,16 @@
+<?php
+
+$userData = [
+    "Harry Potter",
+    "18",
+    "harry@hogwarts.com",
+    "Hogwarts"
+];
+
+list($name, $age, $email, $location) = $userData;
+
+echo "Name: $name<br>";
+echo "Age: $age<br>";
+echo "Email: $email<br>";
+echo "Location: $location<br>";
+
--- resources/views/scriptResult.blade.php
+++ resources/views/scriptResult.blade.php
@@ -1,10 +1,10 @@
 @include('partials.header')
-    <section class="flex flex-col items-center justify-items-center mx-auto space-y-4">
+    <section class="flex flex-col items-center space-y-4">
         <h2 class="text-2xl drop-shadow-lg">Script Result</h1>
-        <div class="flex">
-            <pre class="w-1/2 text-wrap drop-shadow-lg">{{!! $result['output'] !!}}</pre>
+        <div class="flex w-[80rem] h-auto">
+            <pre class="w-1/2 text-wrap drop-shadow-lg h-auto">{{!! $result['output'] !!}}</pre>
             <div class="w-1/2">
-                <pre class="text-white text-wrap text-left leading-loose bg-black shadow-lg rounded-2xl p-2 w-auto h-auto">{{ $styledCode }}</pre>
+                <pre class="text-white text-wrap text-left leading-loose bg-black shadow-lg rounded-2xl p-2 h-auto">{{ $styledCode }}</pre>
             </div>
         </div>
     </section>
